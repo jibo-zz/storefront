5e0e56dad0e685713dbcfb86050cbc9ba6134fa8
Mohamed Omar <m.jiboman@gmail.com>
2023-05-07 18:08:08 +0300

    database sqllit migration

--- store/models.py
+++ store/models.py
@@ -44,9 +44,6 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
 
-    class Meta:
-        ordering = ["first_name", "last_name"]
-
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = "P"
